3bfcdc59b11e759a358cc288ca7c8ab68ce5799d
khloud Hisham <73751574+khloudHisham@users.noreply.github.com>
2024-01-07 22:11:46 +0200

    Add files via upload

---
+++ XO Game/XO Game/Form1.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO_Game
+{
+    public partial class Form1 : Form
+    {
+
+        public enum Player
+        {
+            X, O
+        }
+        //variables
+        Player currentPlayer;  
+        List<Button> buttons; 
+        Random rand = new Random(); 
+        int playerWins = 0; 
+        int computerWins = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetGame();
+
+        }
+
+        
+        private void playerClick(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            currentPlayer = Player.X;
+            button.Text = currentPlayer.ToString();
+            button.Enabled = false;
+            button.BackColor = System.Drawing.Color.Cyan;
+            buttons.Remove(button);
+            Check();
+            AImoves.Start();
+        }
+
+        private void AImove(object sender, EventArgs e)
+        {
+            if (buttons.Count > 0)
+            {
+                int index = rand.Next(buttons.Count);
+                buttons[index].Enabled = false;
+
+                currentPlayer = Player.O;
+                buttons[index].Text = currentPlayer.ToString();
+                buttons[index].BackColor = System.Drawing.Color.DarkSalmon;
+                buttons.RemoveAt(index);
+                Check();
+                AImoves.Stop();
+            }
+        }
+
+        private void restartGame(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+        private void loadbuttons()
+        {
+            buttons = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button9, button8 };
+
+        }
+
+        private void resetGame()
+        {
+
+            foreach (Control X in this.Controls)
+            {
+                if (X is Button && X.Tag == "play")
+                {
+                    ((Button)X).Enabled = true;
+                    ((Button)X).Text = "?";
+                    ((Button)X).BackColor = default(Color);
+                }
+            }
+            loadbuttons();
+        }
+        private void Check()
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X"
+               || button4.Text == "X" && button5.Text == "X" && button6.Text == "X"
+               || button7.Text == "X" && button9.Text == "X" && button8.Text == "X"
+               || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+               || button2.Text == "X" && button5.Text == "X" && button8.Text == "X"
+               || button3.Text == "X" && button6.Text == "X" && button9.Text == "X"
+               || button1.Text == "X" && button5.Text == "X" && button9.Text == "X"
+               || button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                AImoves.Stop(); 
+                MessageBox.Show("Player Wins"); 
+                playerWins++; 
+                label1.Text = "Player Wins- " + playerWins; 
+                resetGame(); 
+            }
+            else if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O"
+            || button4.Text == "O" && button5.Text == "O" && button6.Text == "O"
+            || button7.Text == "O" && button9.Text == "O" && button8.Text == "O"
+            || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+            || button2.Text == "O" && button5.Text == "O" && button8.Text == "O"
+            || button3.Text == "O" && button6.Text == "O" && button9.Text == "O"
+            || button1.Text == "O" && button5.Text == "O" && button9.Text == "O"
+            || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                AImoves.Stop(); // stop the timer
+                MessageBox.Show("Computer Wins"); // show a message box to say computer won
+                computerWins++; // increase the computer wins score number
+                label2.Text = "AI Wins- " + computerWins; // update the label 2 for computer wins
+                resetGame(); // run the reset game
+            }
+
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+       
+
+    }
+}
